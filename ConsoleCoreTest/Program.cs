1bd263b30cfb4d0e1d299538ac9b56e754814159
umudoff <u.amrah@gmail.com>
2020-11-05 02:34:48 +0400

    Add project files.

---
+++ ConsoleCoreTest/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleCoreTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            var name=Console.ReadLine();
+            Console.WriteLine($"Hello, {name}!");
+        }
+    }
+}
